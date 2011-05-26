16b5227809e3c9f52fffa63402079b5bbbfa0e58
Adam Fisk <a@littleshoot.org>
2011-05-25 22:00:45 -0700

    Formatting

--- src/main/java/org/lastbamboo/common/turn/client/TurnClientIoHandler.java
+++ src/main/java/org/lastbamboo/common/turn/client/TurnClientIoHandler.java
@@ -13,68 +13,61 @@ import org.slf4j.LoggerFactory;
 /**
  * {@link IoHandler} for the TURN client connection to the TURN server.
  */
-public class TurnClientIoHandler extends IoHandlerAdapter
-    {
+public class TurnClientIoHandler extends IoHandlerAdapter {
 
     private final Logger LOG = LoggerFactory.getLogger(getClass());
     
-    private final StunMessageVisitor<StunMessage> m_visitor;
+    private final StunMessageVisitor<StunMessage> visitor;
 
     /**
      * Creates a new {@link IoHandler} for the TURN proxy/client.
      * 
      * @param visitor The class for visiting read messages.
      */
-    public TurnClientIoHandler(final StunMessageVisitor<StunMessage> visitor)
-        {
-        m_visitor = visitor;
-        }
+    public TurnClientIoHandler(final StunMessageVisitor<StunMessage> visitor) {
+        this.visitor = visitor;
+    }
 
     @Override
-    public void messageReceived(final IoSession session, final Object message)
-        {
+    public void messageReceived(final IoSession session, final Object message) {
         final StunMessage vsm = (StunMessage) message;
-        vsm.accept(this.m_visitor);
-        }
-    
+        vsm.accept(this.visitor);
+    }
+
     @Override
-    public void sessionClosed(final IoSession session)
-        {
+    public void sessionClosed(final IoSession session) {
         // This is taken care of through an IoServiceListener.
-        }
-    
+    }
+
     @Override
-    public void sessionCreated(final IoSession session)
-        {
+    public void sessionCreated(final IoSession session) {
         SessionUtil.initialize(session);
-        
+
         // We consider the session idle fairly quickly to free up expensive
         // resources.
-        //session.setIdleTime(IdleStatus.WRITER_IDLE, 80);
-        }
+        // session.setIdleTime(IdleStatus.WRITER_IDLE, 80);
+    }
 
     @Override
-    public void sessionIdle(final IoSession session, final IdleStatus status) 
-        {
+    public void sessionIdle(final IoSession session, final IdleStatus status) {
         LOG.debug("Session idle...closing TURN connection to server...");
-        
-        //final AllocateRequest request = new AllocateRequest();
-        //session.write(request);
-        
+
+        // final AllocateRequest request = new AllocateRequest();
+        // session.write(request);
+
         // We used to issue a new allocate request in this case to make sure
-        // the server keeps the TURN connection up.  This only makes sense
-        // for maintaining long-lived TURN connections, though.  In our case,
+        // the server keeps the TURN connection up. This only makes sense
+        // for maintaining long-lived TURN connections, though. In our case,
         // if a connection is idle, it's not being used for a file transfer,
         // so we should close it.
-        
+
         // Note we ideally should not rely on this behavior, instead closing
         // TURN connections whenever we know they're not used.
         session.close();
-        }
-    
-    @Override
-    public void exceptionCaught(final IoSession session, final Throwable cause)
-        {
-        LOG.warn("Caught exception", cause);
-        }
     }
+
+    @Override
+    public void exceptionCaught(final IoSession session, final Throwable cause){
+        LOG.warn("Caught exception", cause);
+    }
+}
